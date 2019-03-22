003a178b8d914d1f79a27f6071d226ee8fafd969
unknown <rmtestard@gmail.com>
2019-03-22 12:15:45 +0100

    installing babel

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -1,3 +1,12 @@
-const config = {};
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    // resolve() checks if the path is correct
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  }
+};
 
 module.exports = config;
